97f7845b9ec3f90998c60c95942f80f217a397c5
Chris Webb <chris.webb@ACCESS-0M8F8JC.uk.access-accounts.com>
2017-12-18 09:28:31 +0000

    Updated all roles to work with new functions

--- role.harvester.js
+++ role.harvester.js
@@ -1,19 +1,14 @@
+//roles
 var roleUpgrader = require('role.upgrader');
+
+//functions
+var decideState = require('function.decideState');
+var drainSource = require('function.drainSource');
 
 module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
-        creep.say(creep.memory.role);
-        // if creep is bringing energy to the spawn or an extension but has no energy left
-        if (creep.memory.working == true && creep.carry.energy == 0) {
-            // switch state
-            creep.memory.working = false;
-        }
-        // if creep is harvesting energy but is full
-        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
-            // switch state
-            creep.memory.working = true;
-        }
+        decideState.run(creep);
         // if creep is supposed to transfer energy to the spawn or an extension
         if (creep.memory.working == true) {
             // find closest spawn or extension which is not full
@@ -42,13 +37,7 @@ module.exports = {
         }
         // if creep is supposed to harvest energy from source
         else {
-            // find closest source
-            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-            // try to harvest energy, if the source is not in range
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                // move towards the source
-                creep.moveTo(source);
-            }
+            drainSource.run(creep);
         }
     }
 };
--- role.repairer.js
+++ role.repairer.js
@@ -1,18 +1,14 @@
+//roles
 var roleBuilder = require('role.builder');
+
+//functions
+var decideState = require('function.decideState');
+var drainSource = require('function.drainSource');
+
 module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
-        creep.say(creep.memory.role);
-        // if creep is trying to repair something but has no energy left
-        if (creep.memory.working == true && creep.carry.energy == 0) {
-            // switch state
-            creep.memory.working = false;
-        }
-        // if creep is harvesting energy but is full
-        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
-            // switch state
-            creep.memory.working = true;
-        }
+        decideState.run(creep);
         // if creep is supposed to repair something
         if (creep.memory.working == true) {
             // find closest structure with less than max hits
@@ -40,13 +36,7 @@ module.exports = {
         }
         // if creep is supposed to harvest energy from source
         else {
-            // find closest source
-            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-            // try to harvest energy, if the source is not in range
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                // move towards the source
-                creep.moveTo(source);
-            }
+            drainSource.run(creep);
         }
     }
 };
