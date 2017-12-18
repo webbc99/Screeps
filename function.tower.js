3333649a32e94b4a5c5a9168355fa5f18c01fe7d
Chris Webb <chris.webb@ACCESS-0M8F8JC.uk.access-accounts.com>
2017-12-18 15:20:17 +0000

    tet

--- function.tower.js
+++ function.tower.js
@@ -37,7 +37,7 @@ module.exports = {
                       filter: (s) => s.hits < s.hitsMax && s.structureType != STRUCTURE_WALL});
 
                     towers.room.visual.text(
-                        'The tower is repairing ' + closestDamagedStructure.structureType.to_s
+                        'The tower is repairing ' + closestDamagedStructure.Structure.structureType
                         towers.pos.x + 1,
                         towers.pos.y,
                         {align: 'left', opacity: 0.8});
