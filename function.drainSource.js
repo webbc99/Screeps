bf5c343414691267b0a4a7a11893a1f9eec08726
Chris Webb <chris.webb@ACCESS-0M8F8JC.uk.access-accounts.com>
2017-12-18 09:18:32 +0000

    Functioned off drainSource

---
+++ function.drainSource.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  run:function(creep){
+
+    var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+    // try to harvest energy, if the source is not in range
+    if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+        // move towards the source
+        creep.moveTo(source);
+    }
+  }
+};
--- role.upgrader.js
+++ role.upgrader.js
@@ -1,4 +1,5 @@
 var decideState = require('function.decideState');
+var drainSource = require('function.drainSource');
 
 module.exports = {
 
@@ -13,13 +14,7 @@ module.exports = {
             }
         }
         else {
-            // find closest source
-            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-            // try to harvest energy, if the source is not in range
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                // move towards the source
-                creep.moveTo(source);
-            }
+            drainSource.run(creep);
         }
 	}
 };
