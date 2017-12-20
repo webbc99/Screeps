f38e9b6097f3f3a36a201e523cd49aba1140af45
Chris Webb <chris.webb@ACCESS-0M8F8JC.uk.access-accounts.com>
2017-12-20 09:40:12 +0000

    attempt to use storage

--- role.harvester.js
+++ role.harvester.js
@@ -20,7 +20,7 @@ module.exports = {
             });
             if (structure == undefined){
               structure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (s) => ((s.structureType == STRUCTURE_CONTAINER) && (s.store[RESOURCE_ENERGY] < s.storeCapacity))
+                filter: (s) => ((s.structureType == STRUCTURE_STORAGE) && (s.store[RESOURCE_ENERGY] < s.storeCapacity))
               });
             }
             // if we found one
